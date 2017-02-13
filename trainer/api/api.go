3295ec90780708b9303c7910f5228591a2b9486e
Michael Lawson <michael.lawson@linewize.com>
2017-02-13 17:26:51 +1300

    authentication work

--- trainer/api/api.go
+++ trainer/api/api.go
@@ -64,7 +64,6 @@ func (api *Api) Init(port int, configurationStore *configuration.ConfigurationSt
 	r.HandleFunc("/audit", api.getAudit)
 	r.HandleFunc("/audit/application", api.getAuditApplication)
 
-
 	r.HandleFunc("/log", api.getLogs)
 	r.HandleFunc("/log/host", api.pushLogs)
 	r.HandleFunc("/log/daemon", api.pushDaemonLogs)
@@ -222,6 +221,7 @@ func (api *Api) hostCheckin(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Token invalid", 403)
 	}
 }
+
 func (api *Api) getLogs(w http.ResponseWriter, r *http.Request) {
 	returnJson(w, nil)
 }
